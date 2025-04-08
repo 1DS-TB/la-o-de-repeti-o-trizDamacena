2f32efe2e05d3355fa93dd743502017b1a717456
trizDamacena <beatrizfdamacena@gmail.com>
2025-04-08 17:00:45 -0300

    Envio da atividade

---
+++ exercicio2.py
@@ -0,0 +1,9 @@
+N = int(input())
+soma = 0
+N += 1
+
+while N > 1:
+    N -= 1
+    soma += N
+    print(N)
+print(soma)
---
+++ exercicio4.py
@@ -0,0 +1,11 @@
+N = int(input())
+expoente = N
+i = N
+while expoente >= 1:
+    expoente = N
+    result = N * expoente
+    print("NUMERO = ", N, "expoente = ", expoente)
+    print(expoente)
+
+    print(result)
+    expoente -= 1
