9826bc8d6a8db46a198b9b971ba0345746f4e160
beaadama <beatriz20familia@gmail.com>
2025-04-13 22:22:50 -0300

    correções6

--- exercicio6.py
+++ exercicio6.py
@@ -1,19 +1,20 @@
-vezes = int(input("Digite o número: "))
+v1 = int(input("Digite o primeiro intervalo: "))
+v2 = int(input("Digite o segundo intervalo: "))
 anterior = 0
 atual = 1
 proximo = 1
 lista = []
-if vezes < 1:
+if v1 < 1 or v2 < 1:
     print("INVALIDO")
 else:
-    for i in range(1, vezes):
+    for i in range(v1, v2):
         lista.append(anterior)
         proximo = atual + anterior
         anterior = atual
         atual = proximo
 
+lista.append(anterior)
 print(lista)
-
 
 
 
