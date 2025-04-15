05ed820c0e7f3f933e7f9d9b8a6b7045c086d7da
Beatriz Damacena <beatrizfdamacena@gmail.com>
2025-04-14 23:10:42 -0300

    Update exercicio5.py

--- exercicio5.py
+++ exercicio5.py
@@ -1,10 +1,13 @@
 numero = int(input("Digite um número: "))
 primo = 0 
-if numero > 1:
-    for i in range(1, numero+1):
-        if numero >= 1 and numero % i == 0:
-            primo += 1
 
-else:
+for i in range(1, numero+1):
+    if numero >= 1 and numero % i == 0:
+        primo += 1
+
+if primo == 2:
+    print(f"{numero} é primo!")
+elif numero < 1 :
     print("INVALIDO")
-
+elif primo >2 or primo == 1 :
+    print(f"{numero} não é primo!")
